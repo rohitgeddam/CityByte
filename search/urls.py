72f1f04771931da8eec18a018e1c7949a70bd815
chaitanya <chaitanya.nagulapalli@accelq.com>
2024-10-31 22:29:42 -0400

    documenting and flake8 search urls.py

--- search/urls.py
+++ search/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
 from .views import city_suggestions, city_photo
-
-urlpatterns = [path("city", city_suggestions, name="city_search"), path("city/photo", city_photo, name="city_photo")]
+urlpatterns = [
+    path("city", city_suggestions, name="city_search"),  # Returns city suggestions based on user input.
+    path("city/photo", city_photo, name="city_photo")  # Fetches a photo of the specified city.
+]

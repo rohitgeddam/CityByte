a720217cb6bcb3d31fb8fd719d275f7927ba4402
chaitanya <chaitanya.nagulapalli@accelq.com>
2024-10-31 22:54:24 -0400

    flake8 and documenting info forms.py

--- info/forms.py
+++ info/forms.py
@@ -3,6 +3,10 @@ from .models import Comment
 
 
 class CommentForm(ModelForm):
+    """
+    A form for creating and updating comments.
+    This form is tied to the Comment model and includes a single field for the comment text.
+    """
     class Meta:
         model = Comment
         fields = ["comment"]

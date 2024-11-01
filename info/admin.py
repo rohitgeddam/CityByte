ae74d2a679e0d99cd6873e3061f68c8e790bd84a
chaitanya <chaitanya.nagulapalli@accelq.com>
2024-10-31 22:59:08 -0400

    flake8 info admin and apps.py

--- info/admin.py
+++ info/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import CitySearchRecord, Comment, FavCityEntry
 
 
+# Registering models to make them accessible in the Django admin interface
 admin.site.register(CitySearchRecord)
 admin.site.register(FavCityEntry)
 admin.site.register(Comment)
--- info/apps.py
+++ info/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class InfoConfig(AppConfig):
+    """
+    Configuration for the 'info' application.
+    This class sets the default auto field type and the application name.
+    """
     default_auto_field = "django.db.models.BigAutoField"
     name = "info"
